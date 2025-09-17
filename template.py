b66cab6cb62ace132ed6613b04af0932f190d024
charan-kilana <charansurya2020@gmail.com>
2025-09-17 02:15:11 -0500

    Update template.yaml 2

--- template.py
+++ template.py
@@ -13,13 +13,13 @@ for dir_ in dirs:
     with open(os.path.join(dir_, ".gitkeep"), "w") as f:
         pass
 
-file = [
+files = [
     "dvc.yaml",
     "params.yaml",
     ".gitignore",
     os.path.join("src"/"__init__.py"),
 ]
 
-for file_ in file:
+for file_ in files:
     with open(file_, "w") as f:
         pass
